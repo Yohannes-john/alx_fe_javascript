c3b185dccf39e31d1b417b6a3f972bf287d23f67
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-10-17 08:22:08 -1200

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -51,16 +51,12 @@ function createAddQuoteForm() {
   `;
   document.body.appendChild(form);
 }
-// Load quotes from localStorage on page load
+
 const storedQuotes = localStorage.getItem("quotes");
 if (storedQuotes) {
   quotes = JSON.parse(storedQuotes);
 }
-
-// Initialize the quote display with the first quote
 showNewQuote();
-
-// Add event listeners for buttons
 document.getElementById("newQuote").addEventListener("click", showNewQuote);
 document.getElementById("quoteDisplay").addEventListener("click", showRandomQuote);
 createAddQuoteForm();
